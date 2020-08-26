a2ca4e1a903de6c0cdaca97a00f7ffa68d14a679
SamarDG <45901327+SamarDG@users.noreply.github.com>
2020-08-26 13:11:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,115 +1,21 @@
-from fastapi import FastAPI
+from flask import Flask
+from flask import request
 
-app = FastAPI()
+app = Flask(__name__)
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+def ML_code(a):
+    return a
 
-
-@app.get("/")
+@app.route('/')
 def home():
-    return [{
-  "type": "graph",
-  "title": "pro json",
-  "gridPos": {
-    "x": 0,
-    "y": 0,
-    "w": 12,
-    "h": 8
-  },
-  "id": 23763571993,
-  "fieldConfig": {
-    "defaults": {
-      "custom": {},
-      "thresholds": {
-        "mode": "absolute",
-        "steps": [
-          {
-            "value": null,
-            "color": "green"
-          },
-          {
-            "value": 80,
-            "color": "red"
-          }
-        ]
-      },
-      "mappings": []
-    },
-    "overrides": []
-  },
-  "pluginVersion": "7.1.4",
-  "renderer": "flot",
-  "yaxes": [
-    {
-      "label": null,
-      "show": true,
-      "logBase": 1,
-      "min": null,
-      "max": null,
-      "format": "short"
-    },
-    {
-      "label": null,
-      "show": true,
-      "logBase": 1,
-      "min": null,
-      "max": null,
-      "format": "short"
-    }
-  ],
-  "xaxis": {
-    "show": true,
-    "mode": "time",
-    "name": null,
-    "values": [],
-    "buckets": null
-  },
-  "yaxis": {
-    "align": false,
-    "alignLevel": null
-  },
-  "lines": true,
-  "fill": 1,
-  "linewidth": 1,
-  "dashLength": 10,
-  "spaceLength": 10,
-  "pointradius": 2,
-  "legend": {
-    "show": true,
-    "values": false,
-    "min": false,
-    "max": false,
-    "current": false,
-    "total": false,
-    "avg": false
-  },
-  "nullPointMode": "null",
-  "tooltip": {
-    "value_type": "individual",
-    "shared": true,
-    "sort": 0
-  },
-  "aliasColors": {},
-  "seriesOverrides": [],
-  "thresholds": [],
-  "timeRegions": [],
-  "datasource": "Prometheus",
-  "targets": [
-    {
-      "expr": "node_memory_Active_bytes{instance=\"localhost:9100\",job=\"node\"}",
-      "legendFormat": "",
-      "interval": "",
-      "refId": "A"
-    }
-  ],
-  "timeFrom": null,
-  "timeShift": null,
-  "fillGradient": 0,
-  "dashes": false,
-  "hiddenSeries": false,
-  "points": false,
-  "bars": false,
-  "stack": false,
-  "percentage": false,
-  "steppedLine": false
-}]
+    return '200 OK!! Connected to Data Source Config Page'
+
+
+#http://127.0.0.1:5000/query/?url=https://google.com
+@app.route('/query/')
+def func1():
+    query = request.args.get('url')
+    return ML_code(query)
+
+if __name__ == '__main__':
+    app.run(debug=True)
